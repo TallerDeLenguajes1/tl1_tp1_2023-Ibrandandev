6de0136bc545eb0517fca40da64bb49d239be8bd
Ignacio Brandán <brandanignaciod@gmail.com>
2023-05-03 20:46:20 -0300

    Termino punto 3e del tp1

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-  printf("hola mundo");
+  int *pEdad, edad = 19;
+  pEdad = &edad;
+
+  printf("hola mundo \n");
+
+  printf("Valor: %d \n", *pEdad);
+  printf("Direccion de Memoria almacenada por el puntero: %d \n", pEdad);
+  printf("Direccion de Memoria de la variable: %d \n", &edad);
+  printf("Direccion de Memoria del puntero: %d \n", &pEdad);
+  printf("Tamaño de memoria utilizado por la variable: %d\n", sizeof(edad));
+  printf("Tamaño de memoria utilizado por el puntero: %d\n", sizeof(pEdad));
 
   return 0;
 }
